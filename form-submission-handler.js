37e19dda460a46a4d61f25c2fd2a846cf3ced5c9
Samson T <samsonfascinate@gmail.com>
2017-10-10 20:34:30 -0700

    test

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -63,7 +63,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
   */
 
   if( !validEmail(data.email) ) {   // if email is not valid show error
-    document.getElementById('email-invalid').style.display = 'block';
+    document.getElementById('email-invalid').style.display = 'none';
     return false;
   } else {
     var url = event.target.action;  //
